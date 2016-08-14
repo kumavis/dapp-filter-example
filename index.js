d2213f2e3a334d1608f8225c7582827be25a49c4
kumavis <aaron@kumavis.me>
2016-08-14 08:56:36 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+console.log('ready')
+// window.addEventListener('load', startApp)
+startApp()
+
+function startApp() {
+
+  console.log('go')
+  var MetaCoin = web3.eth.contract([{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}])
+  var meta = MetaCoin.at('0xfd78216af702ddc3fed7783db1eea14c6fead93d')
+  var events = meta.allEvents()
+  events.watch(function(err, e) {
+    console.log('allEvents!')
+    if (err) {
+      console.log(err)
+    } else {
+      console.log(e.event + ": " + JSON.stringify(e.args))
+    }
+  })
+
+  meta.Transfer({}, { fromBlock: 1420000 }, function (err, result) {
+    console.log('Transfer Event!')
+    console.log(arguments)
+  })
+
+}
+
